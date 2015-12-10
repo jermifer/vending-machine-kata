02f16ce08f73acdafb4f0e924d7eaecfe6e52c56
unknown <jennifer.mankin@EU150805L18.USTROTTING.LOCAL>
2015-12-09 21:29:34 -0500

    pu/setup: WIP, refactored CoinFactory to instantiate dime in a private variable within the createDime method

--- src/main/java/com/pillartechnology/currency/CoinFactory.java
+++ src/main/java/com/pillartechnology/currency/CoinFactory.java
@@ -3,16 +3,16 @@ package com.pillartechnology.currency;
 import java.math.BigDecimal;
 
 public class CoinFactory {
-	private final CoinEntity DIME = new CoinEntity(
-		"Dime",
-		new BigDecimal("0.1"),
-		17.91,
-		1.35,
-		new BigDecimal("2.268")
-	);
-	
 	public CoinEntity createDime() {
-		return DIME;
+		CoinEntity _coin = new CoinEntity(
+			"Dime",
+			new BigDecimal("0.1"),
+			17.91,
+			1.35,
+			new BigDecimal("2.268")
+		);
+		
+		return _coin;
 	}
 }
 

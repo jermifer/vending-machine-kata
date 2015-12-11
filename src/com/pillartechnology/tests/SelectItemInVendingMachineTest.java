861e1cc21c3d01e927560aa532a59e93ee50d651
unknown <jennifer.mankin@EU150805L18.USTROTTING.LOCAL>
2015-12-10 19:47:26 -0500

    pu/tdd: price displays only when $1

---
+++ src/com/pillartechnology/tests/SelectItemInVendingMachineTest.java
@@ -0,0 +1,92 @@
+package com.pillartechnology.tests;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.jmock.Expectations;
+
+public class SelectItemInVendingMachineTest {
+	public interface Display {
+	
+		void displayInventoryItemPrice(Price price);
+	
+	}
+
+	public interface Inventory {
+		
+		void findPrice(String selectedItemName);
+
+	} 
+	
+	public static class Price {
+		
+		private int usCents;
+
+		public Price(int usCents) {
+			this.usCents = usCents;
+		}
+		
+		public static Price usCents(int usCents) {
+			return new Price(usCents);
+		}
+		
+		@Override
+		public boolean equals(Object other) {
+			if( other instanceof Price ) {
+				Price that = (Price) other;
+				return ( this.usCents == that.usCents );
+			} else {
+				return false;
+			}
+		}
+		
+		@Override
+		public int hashCode() {
+			return usCents;
+		}
+		
+		@Override
+		public String toString() {
+			return "a price";
+		}
+		
+	}
+	
+	public static class SaleController {
+
+		private final Display display;
+
+		public SaleController(Inventory inventory, Display display) {
+			this.display = display;
+		}
+
+		public void onSelectItem(String selectedItemName) {
+			this.display.displayInventoryItemPrice(Price.usCents(100));
+		}
+		
+	}
+
+	private Mockery mockery = new Mockery();
+
+	@Test
+	public final void testproductExistsInInventory() {
+		final Inventory inventory = mockery.mock(Inventory.class);
+		final Display display = mockery.mock(Display.class);
+		
+		mockery.checking(new Expectations() {
+			{
+				allowing(inventory).findPrice(with("M&Ms"));
+				will(returnValue(Price.usCents(100)));
+				
+				oneOf(display).displayInventoryItemPrice(Price.usCents(100));
+			}
+		});
+		
+		SaleController saleController = new SaleController(inventory, display);
+		saleController.onSelectItem("M&Ms");
+	}
+}
